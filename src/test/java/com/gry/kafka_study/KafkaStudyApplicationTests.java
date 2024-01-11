c785f0c42d6bf35fe1471d512bfbcc6351a3ff8c
Ayberk Gerey <ayberkgerey@gmail.com>
2024-01-11 20:39:46 +0300

    Initial commit

---
+++ src/test/java/com/gry/kafka_study/KafkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gry.kafka_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
