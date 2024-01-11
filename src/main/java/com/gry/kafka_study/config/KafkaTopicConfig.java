1d58410d203aab308747397d396a5584c3858a7b
Ayberk Gerey <ayberkgerey@gmail.com>
2024-01-11 23:56:52 +0300

    feature: Producer&Topic

---
+++ src/main/java/com/gry/kafka_study/config/KafkaTopicConfig.java
@@ -0,0 +1,18 @@
+package com.gry.kafka_study.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic myTopic(){
+        return TopicBuilder.name("topic1")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+}
